b97e2e97284fe79657f6e055ec7b201770d40fe3
SMNBasheerAnsari <smnbasheeransari2017@gmail.com>
2023-05-30 20:03:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def table_data(request):
+    d={'name':'BA','age':20,'sal':1200,'name1':'BA1','age1':21,'sal1':1000,'name2':'BA2','age2':22,'sal2':1200,'name3':'BA3','age3':23,'sal3':1500}
+    return render(request,'oper.html',context=d)
